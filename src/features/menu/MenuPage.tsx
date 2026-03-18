0828fa5070ffd13c6eb5c08ba6de88fbc0933596
Bramdon Santiago <bramdsantiago@gmail.com>
2026-03-18 15:05:29 -0600

    Deploy Add Menu Game. Game VS IA and Game VS PLAYER

--- src/features/menu/MenuPage.tsx
+++ src/features/menu/MenuPage.tsx
@@ -10,11 +10,11 @@ function MenuPage() {
                 <h1 className='title-game'>Tic <span>Tac</span> Toe</h1>
                 <div className='content-menu'>
                     <div className='btns-menu'>
-                        <button onClick={() => navigate("/game", { state: { vsAI: true } })} className='btn-menu btn-menu-primary'>
+                        <button onClick={() => navigate("/game", { state: { vsAI: true } })} className='btn-menu btn-menu-primary btn-box-shadow-animate'>
                             <h3 className='title-main-btn'><span className='versus-text'>VS</span> IA</h3>
                             <p className='text-description-btn'>Juega contra la IA</p>
                         </button>
-                        <button onClick={() => navigate("/game", { state: { vsAI: false } })} className='btn-menu btn-menu-secondary'>
+                        <button onClick={() => navigate("/game", { state: { vsAI: false } })} className='btn-menu btn-menu-secondary btn-box-shadow-animate'>
                             <h3 className='title-main-btn'><span className='versus-text'>VS</span> OPONENTE</h3>
                             <p className='text-description-btn'>Juega contra un jugador local</p>
                         </button>
